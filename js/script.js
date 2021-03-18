18be93fcb9e5d8447763d770d7a4a161dac34c96
Anna <annazmei2014@gmail.com>
2021-03-18 16:46:08 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		speed:1000,
+		autoplaySpeed:800
+	});
+});
+
+$(document).ready(function(){
+	$('.slider2').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		speed:1000,
+		autoplaySpeed:800
+	});
+});
+
